cb453994941883181058cc4a52fc01e85cc999a5
gnahkyud <khangphamduy6@gmail.com>
2024-10-14 22:06:09 +0700

    noppai
    NOPPAIII

---
+++ Areas/Adim/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23dh111534_MyStore.Areas.Adim.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Adim/Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
